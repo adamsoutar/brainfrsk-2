9221d4af16ac85561cc438bf67aa6c5eb9c1c9f4
adamsoutar <adam@overflo.me>
2022-02-18 01:28:54 +0000

    Read from args instead of hardcoding paths

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 mod codegen;
 mod instructions;
 
+use std::env;
 use codegen::Codegen;
 
 fn main() {
-    let instrs = instructions::read_from_file("test.bf");
+    let filename = env::args().nth(1)
+        .expect("Pass a .bf file path argument");
+    let instrs = instructions::read_from_file(&filename);
 
     if let Ok(i) = instrs {
         let mut gen = Codegen::new(i);
         gen.generate();
         print!("{}", gen.output)
     } else {
-        println!("Failed to lex 'test.bf' {:?}", instrs.err().unwrap())
+        println!("Failed to lex {:?}", instrs.err().unwrap())
     }
 }
